7ba1d48bcf40d2323942e0cd4624f362921e094e
dhitozarfhan <dhitozarfhan@gmail.com>
2025-10-22 11:23:40 +0700

    const db

---
+++ index.js
@@ -0,0 +1,26 @@
+const expres = require('express');
+let mysql = require('mysql2');
+const app = expres ();
+const port = 3001;
+app.use(XPathExpression.json());
+app.use(XPathExpression.urlencondet({ extends: true}));
+
+app.get ("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: '3309', 
+    password: 'Sambeng13',
+    database: 'mahasiswa'
+});
+
+
+
+
